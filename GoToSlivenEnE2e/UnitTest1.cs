7d49b19237fca9196b7ed3f8a643b0cde189525d
HSGeorgiev <hsgeorgiev@gmail.com>
2024-07-19 13:32:01 +0300

    Add project files.

---
+++ GoToSlivenEnE2e/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GoToSlivenEnE2e
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
