a3f7baa74c8d6b7a105267bd5245395c414f41ff
HSGeorgiev <hsgeorgiev@gmail.com>
2024-07-21 11:43:42 +0300

    EventsPage Class added. EventTests Class added with tests.

---
+++ GoToSlivenEnE2e/Pages/EventsPageClass.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoToSlivenEnE2e.Pages
+{
+    internal class EventsPageClass : BaseElementsClass
+    {
+        public EventsPageClass(IWebDriver driver) : base(driver)
+        {
+        }
+
+        private readonly string subUrl = "/event-directory/";
+
+        public void GoToEventsPage()
+        {
+            driver.Navigate().GoToUrl(baseUrl + subUrl);
+        }
+
+    }
+}
---
+++ GoToSlivenEnE2e/Tests/EventsTests.cs
@@ -0,0 +1,117 @@
+﻿using GoToSlivenEnE2e.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoToSlivenEnE2e.Tests
+{
+    internal class EventsTests : BaseTestsClass
+    {
+        public void TestMainMenuLinks()
+        {
+            var eventsPage = new EventsPageClass(driver);
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.AttractionsLinkIsProper(),
+                "'Tourist Attractions' link does not work properly");
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.MapLinkIsProper(),
+                "'Maps' link does not work properly");
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.EventsLinkIsProper(),
+                "'Events' link does not work properly");
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.AccommodationLinkIsProper(),
+                "'Accommodation' link does not work properly");
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.HotelsLinkIsProper(),
+                "'Hotels' link does not work properly");
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.GuestHousesLinkIsProper(),
+                "'Guest Houses' link does not work properly");
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.HolodayHomesLinkIsProper(),
+                "'Holoday Homes' link does not work properly");
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.HutsinkIsProper(),
+                "'Huts & Cabins' link does not work properly");
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.CampingsLinkIsProper(),
+                "'Campings' link does not work properly");
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.RestaurantsLinkIsProper(),
+                "'Restaurants' link does not work properly");
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.PizzeriasLinkIsProper(),
+                "'Pizzerias' link does not work properly");
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.FastFoodLinkIsProper(),
+                "'Fast Food Restaurants' link does not work properly");
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.UsefulLinkIsProper(),
+                "'Useful' link does not work properly");
+        }
+
+        public void TestFooterLinks()
+        {
+            var eventsPage = new EventsPageClass(driver);
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.PrivacyPolicyLinkIsProper(),
+                "'Privacy Policy' link does not work properly");
+
+            eventsPage.GoToEventsPage();
+            Assert.IsTrue(eventsPage.TermsOfUseLinkIsProper(),
+                "'Terms Of Use' link does not work properly");
+
+
+        }
+
+        [Test]
+        public void EventsPage_MainMenu_ShouldBeVisible()
+        {
+            var eventsPage = new EventsPageClass(driver);
+            eventsPage.GoToEventsPage();
+            Assert.That(eventsPage.IsTopMenuDiplayed(), Is.True,
+                "Top menu is not displayed");
+        }
+
+        //[Test]
+        //public void EventsPage_FooterLinks_ShouldBeVisible()
+        //// At the moment of the writing of this test, the links are not included, so the test fails
+        //{
+        //    var eventsPage = new EventsPageClass(driver);
+        //    eventsPage.GoToEventsPage();
+        //    Assert.That(eventsPage.IsFooterLinksDisplayed(), Is.True,
+        //        "Footer links are not displayed");
+        //}
+
+        [Test]
+        public void MainMenu_Links_Should_Works_Properly()
+        {
+            TestMainMenuLinks();
+        }
+
+        //[Test]
+        //public void Footer_Links_Should_Works_Properly()
+        //// At the moment of the writing of this test, the links are not included, so the test fails
+        //{
+        //    TestFooterLinks();
+        //}
+
+    }
+}
+
